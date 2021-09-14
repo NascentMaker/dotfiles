cbf580dc2a06db58d6df093642fe8419d5e2b40c
NascentMaker <hello@nascentmaker.com>
2021-09-14 19:46:13 -0400

    update fonts and nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,11 +1,14 @@
 -- By default title is off. Needed for detecting window as neovim instance (sworkstyle)
 vim.cmd 'set title'
 
--- Plug Initialization
 local Plug = vim.fn['plug#']
+local o = vim.o
+local wo = vim.wo
+local bo = vim.bo
 
 vim.call('plug#begin', '~/.config/nvim/plugged')
 
+Plug 'sheerun/vim-polyglot'
 Plug 'mattn/emmet-vim'
 Plug('cespare/vim-toml', {branch = 'main'})
 Plug 'elzr/vim-json'
@@ -14,22 +17,41 @@ Plug 'sirtaj/vim-openscad'
 Plug 'leafOfTree/vim-svelte-plugin'
 Plug 'digitaltoad/vim-pug'
 Plug 'leafgarland/typescript-vim'
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+Plug 'sainnhe/gruvbox-material'
 
 vim.call('plug#end')
 
--- General settings
-vim.cmd 'set number'
-vim.cmd 'set wrap!'
-vim.cmd 'set expandtab'
-vim.cmd 'set autoindent'
-vim.cmd 'set cindent'
-vim.cmd 'set smartindent'
-vim.cmd 'set nofoldenable'
-vim.cmd 'set tabstop=2'
-vim.cmd 'set shiftwidth=2'
-vim.cmd 'set pastetoggle=<F2>'
-vim.cmd 'set encoding=utf-8'
-vim.cmd 'set noswapfile'
-vim.cmd 'set backspace=indent,eol,start'
-vim.cmd 'set hlsearch'
-vim.cmd 'set incsearch'
+-- Global settings
+o.swapfile = false
+o.smartcase = true
+o.laststatus = 2
+o.hlsearch = true
+o.incsearch = true
+o.ignorecase = false
+
+-- Window settings
+wo.wrap = false
+wo.number = true
+
+-- Buffer settings
+bo.expandtab = true
+bo.cindent = true
+bo.autoindent = true
+bo.smartindent = true
+bo.shiftwidth = 2
+bo.tabstop = 2
+
+-- Airline settings
+vim.g['airline#extensions#tabline#enabled'] = 1
+vim.g['airline_theme'] = 'gruvbox_material'
+vim.g['airline_powerline_fonts'] = 1
+
+-- Theme
+vim.g['gruvbox_material_background'] = 'medium'
+vim.g['gruvbox_material_enable_italic'] = 1
+vim.g['gruvbox_material_disable_italic_comment'] = 1
+vim.g['gruvbox_material_enable_bold'] = 1
+
+vim.cmd 'colo gruvbox-material'
